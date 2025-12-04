df68f8ce57d02da5d30e0376d9a07cb6a8a074ef
yongpresido <youngpresido94@gmail.com>
2025-12-04 17:14:49 -0500

    Clean up AppServiceProvider - remove Redis SSL fix since using database queues/cache

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Redis;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +13,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        
+        //
     }
 }
